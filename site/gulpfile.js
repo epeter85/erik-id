1f97074ac56b213f9f964795397abb3e026de287
Erik Peterson <epeter85@gmail.com>
2016-04-15 16:58:11 -0400

    added foundation and gulp site template

---
+++ site/gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    minifyCSS = require('gulp-minify-css'),
+    concat = require('gulp-concat');
+
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src',
+   'sass/app.scss'
+],
+    env,
+    jsSources = [
+        
+    ],
+    htmlSources = [outputDir + '*.html'],
+    outputDir
+    
+env = process.env.NODE_ENV || 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+} else {
+  outputDir = 'builds/production/';
+}
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulpif(env === 'production', minifyCSS()))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch('builds/development/*.html', ['html']); 
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload())
+});
+
+gulp.task('images', function() {
+    gulp.src('builds/development/images/**/*.*')
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+    .pipe(connect.reload())
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('default', ['html', 'js', 'sass', 'connect', 'images', 'watch']);
